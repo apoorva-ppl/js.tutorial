df45d020b6a34acb4062115554ace969c9092361
Apoorva Pandey <148680480+apoorva-ppl@users.noreply.github.com>
2024-06-22 15:19:24 +0000

    variables declaration

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "apoorva@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+//accountId = 2
+
+
+
+accountEmail =  "hcfc@google.com"
+accountPassword =  "21212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+console.table[accountId, accountEmail,accountPassword,accountCity , accountState] 
+
+/* prefer not to use var becasuse of issue in blog scope and functional scope */
