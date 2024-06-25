283dae78fb4ba9221a2d0e6081247606c7fe415f
Apoorva Pandey <148680480+apoorva-ppl@users.noreply.github.com>
2024-06-25 19:48:45 +0000

    strings -1

---
+++ .devcontainer/01_basics/06_strings.js
@@ -0,0 +1,13 @@
+const name="apoorva"
+const repoCount=50
+
+//console.log(name+repoCount+"Value"); //output:- apoorva50 Value
+
+console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+//modern way of coding and more reliable during backend as compared to above.
+//Output:- Hello my name is apoorva and my repocount is 50 //
+
+
+const gameName = new String{'apoorvap'};
+console.log(gameName [0])//accessing zeroth array //output:-'a'
+//output :-is in string  i.e. "apoorvap"
