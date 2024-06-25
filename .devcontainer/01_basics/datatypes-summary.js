50c809179d46a60fc9e98b84674fe703e7f2025d
Apoorva Pandey <148680480+apoorva-ppl@users.noreply.github.com>
2024-06-25 18:49:21 +0000

    datatype summary -2

--- .devcontainer/01_basics/datatypes-summary.js
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -1,6 +1,37 @@
 //(1)primitive datatypes
 // 7 types: string, number , boolean , null,undefined,symbol,bigInt,
 
+const score=100
+const scoreValue=100.3
+
+const lisLoggedIn=false
+const outsideTemp=null
+let userEmail; //undefined
+
+const id= symbol('123')
+const anotherId= symbol('123')
+
+console.log(id===anotherId);// false
+
+const bigNumber = 34565734562387n // output:-bigInt
+
+
+
+
 
 //(2)Reference datatype (non-primitive)
 //  includes:-Array, objects, funcitons,
+
+const heroes = ["shaktiman", "nagraj", "doga"]
+
+    let myObj={
+    name: "apoorva"
+    age: 22
+}
+
+ const myFunction= function(){
+    console.log("Hello World");
+ }
+
+ console.log(typeof bigNumber); //bigNumber=34565734562387n output= bigInt
+ console.log(typeof scoreValue) //output:-numberj
